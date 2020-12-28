82a03aa381d20f34d5afcbe3a4569ba314c8fc6d
mohamed salah <mohamedsalah.software@gmail.com>
2020-12-28 22:11:12 +0200

    get all prouduct /:page added

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -1,115 +1,132 @@
-import { getLogger } from '@npm-immortal-user/utils/module';
-import { RequestHandler } from 'express';
-import { Types } from 'mongoose';
-import { UserReq } from '../auth/PassportJwt';
-import Product, { ProductDocument, ProductSchema, RecommonedType } from '../model/Product';
-import UserProduct from '../model/UserProduct';
-import { logger } from '../utils/logger';
+import { getLogger } from "@npm-immortal-user/utils/module";
+import { RequestHandler } from "express";
+import { Types } from "mongoose";
+import { UserReq } from "../auth/PassportJwt";
+import Product, {
+  ProductDocument,
+  ProductSchema,
+  RecommonedType,
+} from "../model/Product";
+import UserProduct from "../model/UserProduct";
+import { logger } from "../utils/logger";
 const create: RequestHandler = async (req, res, err) => {
-    try {
-        const {
-            brand,
-            categorie,
-            name,
-            description,
-            image,
-            price,
-            sliderImages,
-            isRecommended
-        }: ProductSchema = req.body;
-        const newProduct: ProductSchema = {
-            brand,
-            categorie,
-            name,
-            description,
-            image,
-            price,
-            sliderImages,
-            isRecommended
-        };
-        const product = await Product.create(newProduct);
-        res.json({ product });
-    } catch (e) {
-        logger.error(e?.message);
-        res.status(501).json({ error: e?.message });
-    }
+  try {
+    const {
+      brand,
+      categorie,
+      name,
+      description,
+      image,
+      price,
+      sliderImages,
+      isRecommended,
+    }: ProductSchema = req.body;
+    const newProduct: ProductSchema = {
+      brand,
+      categorie,
+      name,
+      description,
+      image,
+      price,
+      sliderImages,
+      isRecommended,
+    };
+    const product = await Product.create(newProduct);
+    res.json({ product });
+  } catch (e) {
+    logger.error(e?.message);
+    res.status(501).json({ error: e?.message });
+  }
 };
 const deleteOne: RequestHandler = async (req, res, err) => {
-    try {
-        await Product.deleteOne({ _id: req.body.id });
-        res.json({ sucess: true });
-    } catch (e) {
-        logger.error(e?.message);
-        res.status(501).json({ error: e?.message });
-    }
+  try {
+    await Product.deleteOne({ _id: req.body.id });
+    res.json({ sucess: true });
+  } catch (e) {
+    logger.error(e?.message);
+    res.status(501).json({ error: e?.message });
+  }
 };
 const edit: RequestHandler = async (req, res, err) => {
-    try {
-        const product = await Product.findOneAndUpdate(
-            { _id: req.body.id },
-            { ...req.body },
-            { new: true }
-        );
-        res.json({ sucess: true });
-    } catch (e) {
-        logger.error(e?.message);
-        res.status(501).json({ error: e?.message });
-    }
+  try {
+    const product = await Product.findOneAndUpdate(
+      { _id: req.body.id },
+      { ...req.body },
+      { new: true }
+    );
+    res.json({ sucess: true });
+  } catch (e) {
+    logger.error(e?.message);
+    res.status(501).json({ error: e?.message });
+  }
 };
 const getOne: RequestHandler = async (req, res, err) => {
-    try {
-        const { productId } = req.params;
-        const product = await Product.findOne({ _id: productId });
-        res.json({ product });
-    } catch (e) {
-        logger.error(e?.message);
-        res.status(501).json({ error: e?.message });
-    }
+  try {
+    const { productId } = req.params;
+    const product = await Product.findOne({ _id: productId });
+    res.json({ product });
+  } catch (e) {
+    logger.error(e?.message);
+    res.status(501).json({ error: e?.message });
+  }
 };
 interface Filter {
-    searchBy: string;
-    value: string;
+  searchBy: string;
+  value: string;
 }
 const getAll: RequestHandler = async (req, res, err) => {
-    try {
-        let products = {};
-        if (req.query.searchBy) {
-            let filter: Filter = {
-                value: req.query?.value as string,
-                searchBy: req.query?.searchBy as string
-            };
-            let filters = { [filter.searchBy]: filter.value };
-            products = await Product.find(filters);
-        } else {
-            products = await Product.find();
-        }
-        res.json({ products });
-    } catch (e) {
-        logger.error(e?.message);
-        res.status(501).json({ error: e?.message });
+  try {
+    const { page } = req.params;
+    const limit = 20;
+    const skip = Number(page) * limit;
+    let products = {};
+    if (req.query.searchBy) {
+      let filter: Filter = {
+        value: req.query?.value as string,
+        searchBy: req.query?.searchBy as string,
+      };
+      let filters = { [filter.searchBy]: filter.value };
+      products = await Product.find(filters).limit(limit).skip(skip);
+    } else {
+      products = await Product.find().limit(limit).skip(skip);
     }
+    res.json({ products });
+  } catch (e) {
+    logger.error(e?.message);
+    res.status(501).json({ error: e?.message });
+  }
 };
 const addProductToUser: RequestHandler = async (req: any, res, err) => {
-    try {
-        const { _id }: { _id: string } = req.user as { _id: string };
+  try {
+    const { _id }: { _id: string } = req.user as { _id: string };
 
-        await UserProduct.create({
-            user: Types.ObjectId(req.user._id),
-            product: Types.ObjectId(req.body.productId)
-        });
-        return res.json({ success: true });
-    } catch (e) {
-        logger.error(e?.message);
-        res.status(501).json({ error: e?.message });
-    }
+    await UserProduct.create({
+      user: Types.ObjectId(req.user._id),
+      product: Types.ObjectId(req.body.productId),
+    });
+    return res.json({ success: true });
+  } catch (e) {
+    logger.error(e?.message);
+    res.status(501).json({ error: e?.message });
+  }
 };
 const getUserProducts: RequestHandler = async (req: any, res, err) => {
-    try {
-        const products = await UserProduct.find({ user: req.params.userId }).populate('product');
-        res.json({ products });
-    } catch (e) {
-        logger.error(e?.message);
-        res.status(501).json({ error: e?.message });
-    }
+  try {
+    const products = await UserProduct.find({
+      user: req.params.userId,
+    }).populate("product");
+    res.json({ products });
+  } catch (e) {
+    logger.error(e?.message);
+    res.status(501).json({ error: e?.message });
+  }
 };
-export { create, deleteOne, edit, getOne, getAll, addProductToUser, getUserProducts };
+export {
+  create,
+  deleteOne,
+  edit,
+  getOne,
+  getAll,
+  addProductToUser,
+  getUserProducts,
+};
--- src/route/product.route.ts
+++ src/route/product.route.ts
@@ -13,7 +13,7 @@ import {
 import { UserType } from "../model/User";
 const router = express.Router();
 //PassportJwt.authenticate(['user-rule', 'admin-rule'], { session: false })
-router.get("/", getAll);
+router.get("/all/:page", getAll);
 
 router.patch(
   "/",
