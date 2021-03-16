f80b5d51618da6941c381c088f310f650af05659
mohamed salah <immortal.mind2016@gmail.com>
2021-03-16 11:28:04 +0200

    homepage all product details

--- src/controller/settings.controller.ts
+++ src/controller/settings.controller.ts
@@ -45,10 +45,7 @@ const homePage: RequestHandler = async (req, res, err) => {
     Settings.find({
       $or: [{ name: "CATEGORIES" }, { name: "BRANDS" }, { name: "HOMESLIDER" }],
     }),
-    Product.find(
-      { isRecommended: RecommonedType.YES },
-      "description name price _id image"
-    ).limit(5),
+    Product.find({ isRecommended: RecommonedType.YES }).limit(5),
   ];
   try {
     let results: any[][] = await Promise.all(promiseArray);
